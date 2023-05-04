559a7327215fd970e3b3f6df6e7e8b3e32f67e46
Stephen Nimmo <stephennimmo@gmail.com>
2023-05-04 15:17:40 -0500

    moved to new repo

--- __bootstrap/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-oc apply -f openshift-gitops-operator-subscription.yaml
-oc wait deployment -n openshift-gitops openshift-gitops-server --for condition=Available=True --timeout=90s
